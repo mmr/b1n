47a2d3d59f0fa0957203a22c43b14784aba042b2
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2007-07-15 03:01:10 +0000

    ''

---
+++ b1n-framework/trunk/persistence/src/main/java/b1n/framework/persistence/entity/factory/EntityFactory.java
@@ -0,0 +1,52 @@
+/* Copyright (c) 2007, B1N.ORG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the B1N.ORG organization nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL B1N.ORG OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package b1n.framework.persistence.entity.factory;
+
+import java.util.List;
+import java.util.Map;
+
+import b1n.framework.persistence.entity.Entity;
+import b1n.framework.persistence.entity.EntityNotFoundException;
+
+/**
+ * @author Marcio Ribeiro (mmr)
+ * @created Mar 28, 2007
+ */
+public interface EntityFactory<BoClass extends Entity> {
+    public BoClass getBo();
+
+    public BoClass getBo(Long id) throws EntityNotFoundException;
+
+    public BoClass getBoByQuery(String query) throws EntityNotFoundException;
+
+    public BoClass getBoByQuery(String query, Map<String, ?> params) throws EntityNotFoundException;
+
+    public <T> List<T> getByQuery(String query);
+
+    public <T> List<T> getByQuery(String query, Map<String, ?> params);
+
+    public List<BoClass> getAll();
+}
