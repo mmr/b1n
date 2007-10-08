5edb3ae9eece1ae933c6773cb140d68e93e9768a
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2007-10-08 10:36:49 +0000

    ComboFieldEditor soh funciona no 3.3, usando IntegerFieldEditor por enqto

--- InsertBeautifier/trunk/src/org/b1n/ib/preferences/PreferencesPage.java
+++ InsertBeautifier/trunk/src/org/b1n/ib/preferences/PreferencesPage.java
@@ -1,34 +1,14 @@
-/* Copyright (c) 2007, B1N.ORG
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the B1N.ORG organization nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL B1N.ORG OR ITS CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (c) 2007, B1N.ORG All rights reserved. Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution. * Neither the name of the B1N.ORG organization nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL B1N.ORG OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.b1n.ib.preferences;
 
 import org.b1n.ib.InsertBeautifierPlugin;
 import org.eclipse.jface.preference.ColorFieldEditor;
-import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -38,11 +18,24 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
  * @date 07/10/2007
  */
 public final class PreferencesPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+    /** Mínimo para configuração de campos mínimos para formatação. */
+    private static final int MIN_FIELDS_MIN = 1;
+
+    /** Máximo para configuração de campos mínimos para formatação. */
+    private static final int MIN_FIELDS_MAX = 10;
+
     /** Dados de combo para minimo de campos. */
     private static final String[][] DATA;
 
     static {
-        DATA = new String[][] { { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" }, { "5", "5" }, { "6", "6" }, { "7", "7" }, { "8", "8" }, { "9", "9" }, { "10", "10" } };
+        // Popula array bidimensional com dados de combo
+        int items = MIN_FIELDS_MAX - MIN_FIELDS_MIN + 1;
+
+        DATA = new String[items][2];
+        for (int i = 0, j = MIN_FIELDS_MIN; i < items; i++, j++) {
+            DATA[i][0] = String.valueOf(j);
+            DATA[i][1] = String.valueOf(j);
+        }
     }
 
     /**
@@ -59,11 +52,16 @@ public final class PreferencesPage extends FieldEditorPreferencePage implements 
      */
     @Override
     public void createFieldEditors() {
-        this.addField(new ComboFieldEditor(PreferencesConstants.MIN_FIELDS, "&Mínimo de Campos: ", PreferencesPage.DATA, this.getFieldEditorParent()));
+        // XXX (mmr) : ComboFieldEditor soh funciona em 3.3, comentado por enquanto
+        // this.addField(new ComboFieldEditor(PreferencesConstants.MIN_FIELDS, "&Mínimo de Campos: ", PreferencesPage.DATA, this.getFieldEditorParent()));
 
-        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_INSERT, "&Insert", this.getFieldEditorParent()));
-        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_COMMENT, "&Comentário", this.getFieldEditorParent()));
-        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_STRING, "&String", this.getFieldEditorParent()));
+        IntegerFieldEditor minFields = new IntegerFieldEditor(PreferencesConstants.MIN_FIELDS, "&Mínimo de Campos:", this.getFieldEditorParent(), 2);
+        minFields.setValidRange(MIN_FIELDS_MIN, MIN_FIELDS_MAX);
+        this.addField(minFields);
+
+        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_INSERT, "&Insert:", this.getFieldEditorParent()));
+        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_COMMENT, "&Comentário:", this.getFieldEditorParent()));
+        this.addField(new ColorFieldEditor(PreferencesConstants.COLOR_STRING, "&String:", this.getFieldEditorParent()));
     }
 
     /**
