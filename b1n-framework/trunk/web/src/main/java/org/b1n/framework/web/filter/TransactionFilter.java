8a4c54d39d5281015b1280f473ee2550cbc53c94
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2008-05-02 23:14:06 +0000

    coisas do web

--- b1n-framework/trunk/web/src/main/java/org/b1n/framework/web/filter/TransactionFilter.java
+++ b1n-framework/trunk/web/src/main/java/org/b1n/framework/web/filter/TransactionFilter.java
@@ -23,7 +23,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.b1n.framework.web;
+package org.b1n.framework.web.filter;
 
 import java.io.IOException;
 
