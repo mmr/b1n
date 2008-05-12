89c8cb8fdd8ee1850e370bd1872c01d498e0b742
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2008-05-12 03:14:00 +0000

    notifier mozilla extension

---
+++ notifier/trunk/defaults/preferences/notifier.js
@@ -0,0 +1,5 @@
+pref("extensions.notifier.boolpref", false);
+pref("extensions.notifier.intpref", 0);
+pref("extensions.notifier.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.notifier@b1n.org.description", "chrome://notifier/locale/notifier.properties");
