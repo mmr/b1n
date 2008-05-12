ad8fd319d6af5306419d9133d141ad36d6133b7d
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2008-05-12 06:33:52 +0000

    lala

--- notifier/trunk/defaults/preferences/notifier.js
+++
@@ -1,4 +0,0 @@
-pref("org.b1n.notifier.register_url", "http://localhost/notifier/register);
-
-// See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.notifier@b1n.org.description", "chrome://notifier/locale/notifier.properties");
