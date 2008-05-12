a93694620f7ece9bdbc7844550a8166826c5e2cb
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2008-05-12 03:35:32 +0000

    register url

--- notifier/trunk/defaults/preferences/notifier.js
+++ notifier/trunk/defaults/preferences/notifier.js
@@ -1,5 +1,4 @@
-pref("extensions.notifier.boolpref", false);
-pref("extensions.notifier.intpref", 0);
-pref("extensions.notifier.stringpref", "A string");
+pref("org.b1n.notifier.register_url", "http://localhost/notifier/register);
+
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.notifier@b1n.org.description", "chrome://notifier/locale/notifier.properties");
