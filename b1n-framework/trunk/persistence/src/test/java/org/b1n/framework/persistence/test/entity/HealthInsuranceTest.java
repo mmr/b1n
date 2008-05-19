9625b7068ec4d0c67a4daa7866ad3bc3b5cad126
mribeiro <mribeiro@e3e3b024-de34-0410-b669-3befe1573b09>
2008-05-19 00:24:50 +0000

    1) factory > dao 2) test

---
+++ b1n-framework/trunk/persistence/src/test/java/org/b1n/framework/persistence/test/entity/HealthInsuranceTest.java
@@ -0,0 +1,12 @@
+package org.b1n.framework.persistence.test.entity;
+
+import org.b1n.framework.persistence.entity.HealthInsurance;
+import org.b1n.framework.persistence.test.EntityTestCase;
+
+/**
+ * @author Marcio Ribeiro
+ * @date May 18, 2008
+ */
+public class HealthInsuranceTest extends EntityTestCase<HealthInsurance> {
+
+}
